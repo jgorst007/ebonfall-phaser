a39bdfba30538f19317df1dc4ff9e7ff073fe001
jgorst007 <54079352+jgorst007@users.noreply.github.com>
2025-06-14 23:52:51 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -20,8 +20,8 @@ let player, cursors;
 
 function preload() {
     this.load.spritesheet('hero', 'assets/sprite.png', {
-        frameWidth: 32,
-        frameHeight: 32
+        frameWidth: 337,
+        frameHeight: 337
     });
 }
 
@@ -29,8 +29,8 @@ function create() {
     player = this.physics.add.sprite(200, 150, 'hero', 0);
 
     this.anims.create({
-        key: 'walk-down',
-        frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 3 }),
+        key: 'walk',
+        frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 0 }),
         frameRate: 10,
         repeat: -1
     });
@@ -41,19 +41,20 @@ function create() {
 function update() {
     player.setVelocity(0);
 
-    if (cursors.left.isDown) {
-        player.setVelocityX(-100);
-        player.anims.play('walk-down', true);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(100);
-        player.anims.play('walk-down', true);
-    } else if (cursors.up.isDown) {
-        player.setVelocityY(-100);
-        player.anims.play('walk-down', true);
-    } else if (cursors.down.isDown) {
-        player.setVelocityY(100);
-        player.anims.play('walk-down', true);
+    if (cursors.left.isDown || cursors.right.isDown || cursors.up.isDown || cursors.down.isDown) {
+        player.anims.play('walk', true);
     } else {
         player.anims.stop();
+    }
+
+    if (cursors.left.isDown) {
+        player.setVelocityX(-100);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(100);
+    }
+    if (cursors.up.isDown) {
+        player.setVelocityY(-100);
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(100);
     }
 }
